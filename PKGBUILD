3ca6902655a86480e7ccb9713b9fa7fd3648e30d
Sean Sullivan <22581534+sullivan-sean@users.noreply.github.com>
2020-09-28 00:03:26 -0400

    update colorschemes and pkgbuild

--- PKGBUILD
+++ PKGBUILD
@@ -8,23 +8,21 @@ arch=('i686' 'x86_64' 'armv7h')
 url="https://github.com/sullivan-sean/st"
 license=('MIT')
 depends=(libxft)
-source=("git://github.com/sullivan-sean/st")
-sha1sums=('SKIP')
-
-prepare() {
-  cd "$srcdir"
-}
+source=('git://github.com/sullivan-sean/st'
+				'config.h')
+sha1sums=('SKIP' 'SKIP')
 
 build() {
   cd "$srcdir/${pkgname}"
+  cp "$srcdir/config.h" config.h
   make X11INC=/usr/include/X11 X11LIB=/usr/lib/X11
 }
 
 package() {
   local installopts='--mode 0644 -D'
   cd "$srcdir/${pkgname}"
-	make PREFIX=/usr DESTDIR="$pkgdir/" install
+  make PREFIX=/usr DESTDIR="$pkgdir/" install
   install $installopts LICENSE "${pkgdir}/usr/share/licenses/${pkgname}/LICENSE"
-	install $installopts README "${pkgdir}/usr/share/doc/${pkgname}/README"
+  install $installopts README "${pkgdir}/usr/share/doc/${pkgname}/README"
 }
 
--- config.h
+++ config.h
@@ -98,42 +98,63 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-	/* 8 normal colors */
-	"#222222",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+static const char *gruvbox[] = {
+  /* 8 normal colors */
+  [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+  [1] = "#cc241d", /* red     */
+  [2] = "#98971a", /* green   */
+  [3] = "#d79921", /* yellow  */
+  [4] = "#458588", /* blue    */
+  [5] = "#b16286", /* magenta */
+  [6] = "#689d6a", /* cyan    */
+  [7] = "#a89984", /* white   */
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  /* 8 bright colors */
+  [8]  = "#928374", /* black   */
+  [9]  = "#fb4934", /* red     */
+  [10] = "#b8bb26", /* green   */
+  [11] = "#fabd2f", /* yellow  */
+  [12] = "#83a598", /* blue    */
+  [13] = "#d3869b", /* magenta */
+  [14] = "#8ec07c", /* cyan    */
+  [15] = "#ebdbb2", /* white   */
+};
+
+static const char *nord[] = {
+	"#3b4252", /* black   */
+	"#bf616a", /* red     */
+	"#a3be8c", /* green   */
+	"#ebcb8b", /* yellow  */
+	"#81a1c1", /* blue    */
+	"#b48ead", /* magenta */
+	"#88c0d0", /* cyan    */
+	"#e5e9f0", /* white   */
+
+	"#4c566a", /* black   */
+	"#bf616a", /* red     */
+	"#a3be8c", /* green   */
+	"#ebcb8b", /* yellow  */
+	"#81a1c1", /* blue    */
+	"#b48ead", /* magenta */
+	"#8fbcbb", /* cyan    */
+	"#eceff4", /* white   */
 
 	[255] = 0,
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+	"#2e3440", /* background */
+	"#d8dee9", /* foreground */
 };
+
+static const char **colorname = gruvbox;
 
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
+unsigned int defaultfg = 15;
 unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+static unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 
 /*
